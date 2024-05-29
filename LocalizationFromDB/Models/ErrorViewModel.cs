3d18691dd19da301f5704a7c8ea137a27f7c8f8e
Mert Can Kasapoğlu <mkasapoglu@guralcloud.com>
2024-05-29 08:59:49 +0300

    Add project files.

---
+++ LocalizationFromDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocalizationFromDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
